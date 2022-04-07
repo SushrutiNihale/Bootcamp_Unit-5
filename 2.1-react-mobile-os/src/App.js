39b325b71180ba0d1209b35f66715fd078e41926
Sushruti Nihale <sushrutin6@gmail.com>
2022-04-07 21:12:00 +0530

    adding completed 2.1 project

---
+++ 2.1-react-mobile-os/src/App.js
@@ -0,0 +1,30 @@
+import './App.css';
+
+function App() {
+  let title1 = "Mobile Operating System";
+  let title2 = "Mobile Manufacturers";
+  let list1 = ["Android","Blackberry","IPhone","Windows Phone"];
+  let list2 = ["Samsung","HTC","Micromax","Apple"];
+
+  return (
+    <div className="App">
+      <Comp title={title1} items={list1}/>
+      <Comp title={title2} items={list2}/>
+    </div>
+  );
+}
+
+const Comp = (props) => {
+  return (
+    <div>
+      <h3>{props.title}</h3>
+      <ul>
+        {props.items.map((e) => {
+          return <li>{e}</li>
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default App;
